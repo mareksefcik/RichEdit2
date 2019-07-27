b8bdb6cad709cfe9bf8a4ae970c8a47b82ce59b3
Marek Sefcik <marek.sefcik.2@gmail.com>
2019-07-27 11:01:25 +0200

    ...

---
+++ pull-request.cpp
@@ -0,0 +1,39 @@
+void Tab1::OnAdd()
+{
+	UpdateData(TRUE);					// update member		
+	m_listBox.AddString(m_EditString);
+	//UpdateData(FALSE);				// update gui
+}
+
+void Tab1::OnButton1()
+{
+	UpdateData(TRUE); // gui > member
+	m_sText1.Format(_T("Check1 : %d, Check2 : %d"), m_bCheck1, m_bCheck2);
+	UpdateData(FALSE); // member > gui
+}
+
+void Tab1::OnGet()
+{
+	int index;
+	CString strText;
+	index = m_listBox.GetCurSel();
+	m_listBox.GetText(index, strText);
+	MessageBox(strText);
+}
+
+void Tab1::OnRemove()
+{
+	int index;
+	CString strText;
+	index = m_listBox.GetCurSel();
+	m_listBox.DeleteString(index);
+}
+
+void Tab1::OnSelChangeLB()
+{
+	int index;
+	CString strText;
+	index = m_listBox.GetCurSel();
+	m_listBox.GetText(index, strText);
+	//MessageBox(strText);
+}
