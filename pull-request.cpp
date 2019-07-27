3a89b933d29f88d243af4bf4b6a0c9fd9b4cce6b
Marek Sefcik <marek.sefcik.2@gmail.com>
2019-07-27 14:42:46 +0200

    Update pull-request.cpp

--- pull-request.cpp
+++ pull-request.cpp
@@ -3,8 +3,9 @@ void Tab1::OnAdd()
 	UpdateData(TRUE);					// update member		
 	m_listBox.AddString(m_EditString);
 	//UpdateData(FALSE);				// update gui
-	int x = 1;
-	x++;
+	int y = 1;
+	y++;
+	y++;
 }
 
 void Tab1::OnButton1()
