c1f5dbcf0dc93da6b8589fbdff0fa3d06f8e21c8
Marek Sefcik <marek.sefcik.2@gmail.com>
2019-07-29 10:45:42 +0200

    ...

--- b.cpp
+++ b.cpp
@@ -1,10 +1,4 @@
 void Tab1::OnAdd()
 {
-	UpdateData(TRUE);					// update member		
-	m_listBox.AddString(m_EditString);
-	//UpdateData(FALSE);				// update gui
-	int y = 1;
-	y++;
-	y++;
-	y++;
+	int b = 1;
 }
